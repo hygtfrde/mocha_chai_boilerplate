5d38630e6a38b0f3b3de3f5ca64159401088f828
bhu <harold9188@gmail.com>
2021-10-04 17:20:23 -0700

    deepEqual and notDeepEqual

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -46,9 +46,10 @@ suite('Unit Tests', function () {
       assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
+    // this is some JavaScript weirdness 
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
+      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
     });
   });
 
